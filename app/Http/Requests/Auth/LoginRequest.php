029d94e74fd4fc6212dc42264c1162ca37564a28
Luca Lenoci <lucaxl10@gmail.com>
2025-05-26 23:09:18 +0200

    Modificato i messaggi di errore di autenticazione per fornire feedback in italiano

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -45,7 +45,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => trans('auth.failed'),
+                'email' => 'Email o Password errati. Riprova.',
             ]);
         }
 
@@ -68,10 +68,7 @@ class LoginRequest extends FormRequest
         $seconds = RateLimiter::availableIn($this->throttleKey());
 
         throw ValidationException::withMessages([
-            'email' => trans('auth.throttle', [
-                'seconds' => $seconds,
-                'minutes' => ceil($seconds / 60),
-            ]),
+            'email' => "Hai effettuato troppi tentativi di accesso. Per favore riprova tra {$seconds} secondi (circa " . ceil($seconds / 60) . " minuti).",
         ]);
     }
 
